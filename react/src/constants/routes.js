fc263aa9e82dabd40c5358c74bb59cb134acba1e
leohlchan <leohlchan@gmail.com>
2024-10-12 19:47:30 +0800

    Change route and component name

--- react/src/constants/routes.js
+++ react/src/constants/routes.js
@@ -2,6 +2,7 @@ export const ROUTES = {
     LOGIN: '/login',
     REGISTER: '/registration',
     LOGIN_CALLBACK: '/login/callback',
+    GOOGLE_LOGIN_CALLBACK: '/auth/google/callback',
     HOME: '/',
     DASHBOARD: '/dashboard',
     FORGET_PASSWORD: '/forget_password',
--- react/src/pages/GoogleCallback.js
+++ react/src/pages/GoogleCallback.js
@@ -5,7 +5,7 @@ import { useAuthContext } from "../hook/AuthContext";
 import { ROUTES } from "../constants/routes";
 import LoadingPage from "./LoadingPage";
 
-const LoginCallback = () => {
+const GoogleLoginCallback = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const {handleGoogleCallback}= useAuthContext();
@@ -36,4 +36,4 @@ const LoginCallback = () => {
 
 }
 
-export default LoginCallback;
+export default GoogleLoginCallback;
